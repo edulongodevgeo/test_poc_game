7d61b9aaf8688bbe930430ca09000102fe417aca
Eduardo Longo <65884093+edulongodevgeo@users.noreply.github.com>
2025-03-31 15:40:12 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1743431703,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/jogador-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/tilemap.png",
+		"images/presa-sheet0.png",
+		"images/tiledbackground.png",
+		"images/moldura_cronometro-sheet0.png",
+		"images/colisor-sheet0.png",
+		"media/15_voy_synth.m4a",
+		"media/15_voy_synth.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
